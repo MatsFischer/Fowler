ab3f34846b80a26ec1d8ae5953b9390abaf10bde
mfischer <fischer@econda.de>
2023-05-10 20:47:07 +0200

    added first tests for Customer

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,5 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
+    private Customer customer;
 
+    @BeforeEach
+    public void setUp() {
+        customer = new Customer("Sarah");
+    }
+
+    @Test
+    @DisplayName("Get Name")
+    public void getName() {
+        assertEquals("Sarah", customer.getName());
+    }
 }
