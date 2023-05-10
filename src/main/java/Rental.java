c77e5bd099c7828d68e9ba574aca1112858d5ed1
mfischer <fischer@econda.de>
2023-05-10 21:45:15 +0200

    moved getFrequentRenterPoints method to Movie

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -13,12 +13,7 @@ class Rental {
     }
 
     int getFrequentRenterPoints() {
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            return 2;
-        else {
-            return 1;
-        }
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 
     double getCharge() {
